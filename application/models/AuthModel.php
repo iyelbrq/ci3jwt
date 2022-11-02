c941e216597b63168e22ee85dac1e9d12eb064aa
iyelbrq <msuhail2711@gmail.com>
2022-11-02 17:00:34 +0700

    ci3JWT new

--- application/models/AuthModel.php
+++ application/models/AuthModel.php
@@ -5,7 +5,7 @@ class AuthModel extends CI_Model
 
     function check_login($username, $password)
     {
-        $query = $this->db->query("SELECT * FROM user WHERE username='$username' AND password='$password' ");
+        $query = $this->db->query("SELECT * FROM fs_login WHERE username='$username' AND password='$password' ");
         if ($query->num_rows() > 0) {
             return $query->result_array();
         } else {
